c2de97b27a3e804e883926926e10cf1bdffd84c2
Md Nishat Miah <nishat00101@gmail.com>
2026-04-29 22:42:18 +0600

    Update table-search

--- resources/js/components/table-search.tsx
+++ resources/js/components/table-search.tsx
@@ -1,5 +1,6 @@
 import { router } from '@inertiajs/react';
 import { X } from 'lucide-react';
+import { useMemo } from 'react';
 import { Button } from './ui/button';
 import { Input } from './ui/input';
 
@@ -7,12 +8,22 @@ interface FilterProps {
     search?: string;
     perPage?: string;
 }
+
 interface Props {
     route: string;
     data: FilterProps;
     setData: (key: keyof FilterProps, value: string) => void;
     placeHolderMsg?: string;
     delay?: number;
+}
+
+function debounce<T extends (...args: any[]) => void>(fn: T, delay: number) {
+    let timeout: ReturnType<typeof setTimeout>;
+
+    return (...args: Parameters<T>) => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => fn(...args), delay);
+    };
 }
 
 const TableSearch = ({
@@ -22,26 +33,21 @@ const TableSearch = ({
     placeHolderMsg = 'Users',
     delay = 200,
 }: Props) => {
-    const debounce = (fn: Function, delay: number) => {
-        let timeout: ReturnType<typeof setTimeout>;
+    const searchRequest = useMemo(
+        () =>
+            debounce((value: string) => {
+                const query = {
+                    ...(value && { search: value }),
+                    ...(data.perPage && { perPage: data.perPage }),
+                };
 
-        return (...args: any[]) => {
-            clearTimeout(timeout);
-            timeout = setTimeout(() => fn(...args), delay);
-        };
-    };
-
-    const searchRequest = debounce((value: string) => {
-        const query = {
-            ...(value && { search: value }),
-            ...(data.perPage && { perPage: data.perPage }),
-        };
-
-        router.get(route, query, {
-            preserveState: true,
-            preserveScroll: true,
-        });
-    }, delay);
+                router.get(route, query, {
+                    preserveState: true,
+                    preserveScroll: true,
+                });
+            }, delay),
+        [route, data.perPage, delay],
+    );
 
     const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
         const value = e.target.value;
@@ -58,7 +64,7 @@ const TableSearch = ({
     return (
         <div className="relative w-1/3">
             <Input
-                value={data.search}
+                value={data.search || ''}
                 onChange={handleSearch}
                 className="h-10 pr-10"
                 type="text"
@@ -69,7 +75,7 @@ const TableSearch = ({
             <Button
                 onClick={handleReset}
                 className="absolute top-1/2 right-2 size-8 -translate-y-1/2 rounded bg-red-500 p-1 hover:bg-red-600"
-                disabled={data.search === ''}
+                disabled={!data.search}
             >
                 <X size={16} />
             </Button>
